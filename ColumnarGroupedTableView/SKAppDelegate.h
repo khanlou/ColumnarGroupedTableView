52b49ddc023e065c072ea02d850579959dfda46d
Soroush Khanlou <soroushkhanlou@gmail.com>
2013-04-29 20:43:30 -0400

    initial code

---
+++ ColumnarGroupedTableView/SKAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SKAppDelegate.h
+//  ColumnarGroupedTableView
+//
+//  Created by Soroush Khanlou on 4/29/13.
+//  Copyright (c) 2013 Planetary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SKViewController;
+
+@interface SKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) SKViewController *viewController;
+
+@end
---
+++ ColumnarGroupedTableView/SKViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKViewController.h
+//  ColumnarGroupedTableView
+//
+//  Created by Soroush Khanlou on 4/29/13.
+//  Copyright (c) 2013 Planetary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKViewController : UIViewController
+
+@end
